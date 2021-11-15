1c9653b5d2d74a33b0a89cdef56289b9cd0e9a2d
hassanrobleh <hassan.alirobleh@gmail.com>
2021-11-15 23:51:04 +0100

    Add props-types

--- src/pages/Freelances/index.jsx
+++ src/pages/Freelances/index.jsx
@@ -1,9 +1,34 @@
+import Card from '../../components/Card'
+import DefaultPicture from '../../assets/profil.png'
+
+
+const freelances = [
+    {
+        name: 'Jane Doe',
+        jobTitle: 'Devops',
+        picture: DefaultPicture,
+    },
+    {
+        name: 'John Doe',
+        jobTitle: 'Developpeur frontend',
+        picture: DefaultPicture,
+    },
+    {
+        name: 'Jeanne Biche',
+        jobTitle: 'Développeuse Fullstack',
+        picture: DefaultPicture,
+    },
+]
+
 
 export default function Freelances() {
 
     return (
         <div>
-            <span>Freelances</span>
+            <h1>Freelances</h1>
+            {freelances.map((profil, index) => (
+                <Card key={`${profil.name}-${index}`} title={42}  label={profil.jobTitle} picture={profil.picture}/>
+            ))}
         </div>
     )
 }
