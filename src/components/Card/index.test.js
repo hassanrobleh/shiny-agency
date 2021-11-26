76472dfc7629976c31f6a21a7266e427bf266b39
hassanrobleh <hassan.alirobleh@gmail.com>
2021-11-26 21:46:38 +0100

    add test card

--- src/components/Card/index.test.js
+++ src/components/Card/index.test.js
@@ -1,5 +1,5 @@
 import Card from './'
-import { render, screen } from '@testing-library/react'
+import { render, screen, fireEvent } from '@testing-library/react'
 import { ThemeProvider } from '../../utils/context'
 
 describe('Card', () => {
@@ -18,5 +18,22 @@ describe('Card', () => {
     const cardTitle = screen.getByText(/Harry/i)
     expect(cardPicture.src).toBe('http://localhost/myPicture.png')
     expect(cardTitle.textContent).toBe(' Harry Potter ')
+  })
+
+  it('Should add star around title', async () => {
+    render(
+      <ThemeProvider>
+        <Card
+          title="Harry Potter"
+          label="Magicien frontend"
+          picture="/myPicture.png"
+        />
+      </ThemeProvider>
+    )
+
+    const cardTitle = screen.getByText(/Harry/i)
+    const parentNode = cardTitle.closest('div')
+    fireEvent.click(parentNode)
+    expect(cardTitle.textContent).toBe('⭐️ Harry Potter ⭐️')
   })
 })
